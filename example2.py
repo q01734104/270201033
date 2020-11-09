c6c62bc73619bf7f1eb1fa26b454fe40a3b53fb1
q01734104 <73104503+q01734104@users.noreply.github.com>
2020-11-09 12:42:29 +0300

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,11 @@
+year=int("Enter the year:")
+if year%400==0:
+  print("Leap year")
+elif year%100==0:
+  print("Leap year")
+elif year%4==0:
+  print("Leap year")
+else:
+  print("Not a Leap year")
+
+  
---
+++ example5.py
@@ -0,0 +1,5 @@
+a=int(input())
+x=1
+for i in range(a,0,-1):
+  x=x*i
+print(x)
