d7c41c1dd533a571fd51ffda6f57b20a6130685c
q01734104 <73104503+q01734104@users.noreply.github.com>
2020-11-09 12:55:57 +0300

    Add files via upload

---
+++ lab4/example3.py
@@ -0,0 +1,6 @@
+nums = [8, 60, 43, 55, 25, 134, 1]
+sum=0
+for i in nums:
+  sum += i
+print(i)
+# print(sum(nums))
---
+++ lab4/example4.py
@@ -0,0 +1,6 @@
+a=int(input())
+b=int(input())
+x=1
+for i in range(b):
+  x=x*a
+print(x)
